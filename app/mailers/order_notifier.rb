44513f074341a352b7481681cada30dce3caa4fd
Edgard R. Navarrete <kinglouis.belmont@gmail.com>
2013-08-26 19:58:07 -0700

    Added a mailer for send ing emails to customers.

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,27 @@
+class OrderNotifier < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.recieved.subject
+  #
+  def recieved(order)
+    @order = order
+
+    mail to: order.email subject: 'Pragmatic Store Order Confirmation'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+
+    mail to: order.email, subject: 'Pragmatic Store Order shipped'
+  end
+end
+
+
